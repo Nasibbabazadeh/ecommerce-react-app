2e10af60ef24bd7cc9c8e4ea3a022d4b96f37abc
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-11-15 09:19:23 +0400

    redux & persist & store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Provider } from "react-redux";
 import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/HomePage";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 
 function App() {
   return (
-    <>
-      <MainLayout children={<HomePage />} />
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MainLayout children={<HomePage />} />
+      </PersistGate>
+    </Provider>
   )
 }
 
